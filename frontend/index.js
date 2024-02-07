83314a72b6cd2f51c91a6edcce0bdb8af89a0a87
josecobi <58313777+josecobi@users.noreply.github.com>
2024-02-06 19:17:15 -0500

    fixed references in the model schemas.Simple get routes work well. Added views using Ejs and added buttons and forms; still unfinished. Added index.js and helpers to the frontend, still unfinished

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+// LOGIC FOR THE GET LINKS FUNCTION
+// Variables 
+const getLinksBtn = document.querySelector(".getLinksBtn");
+const listOfLinks = document.querySelector(".list-of-links");
+const tableBody = document.querySelector('.table-body');
+const originalLink = document.querySelector('.original-link')
+// Call function to get list of links
+getList();
--- server.mjs
+++ server.mjs
@@ -3,29 +3,54 @@ dotenv.config();
 
 import express from "express";
 import mongoose from "mongoose";
-import User  from './models/usersmodel.mjs';
-import Url from './models/urlmodel.mjs';
-import Qrcode from './models/qrcode.mjs';
+import User  from './backend/models/usersmodel.mjs';
+import Url from './backend/models/urlmodel.mjs';
+import Qrcode from './backend/models/qrcode.mjs';
+import ejs from "ejs";
 const app = express();
 
 await mongoose.connect(process.env.ATLAS_URI);
-
-app.get("/", (req, res) => {
-    res.status(200).send("<h1>Wellcome to the urlShortner</h1>");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.on("open", () => {
+    console.log("Database connected");
 });
 
-app.get("/users", async (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//set EJS engine
+app.set("view engine", "ejs");
+app.set("views", "./backend/views");
+//serve static files
+app.use(express.static("frontend"));
+
+
+app.get("/", (req, res) => {
+    res.status(200).render("index");
+});
+
+app.get("/users", (req, res) => {
+  res.status(200).render("users");
+});
+
+app.get("/links", (req, res) => {
+  res.status(200).render("links");
+});
+app.get("/qrcodes", (req, res) => {
+  res.status(200).render("qrcodes");
+});
+app.get("/allusers", async (req, res) => {
 const users = await User.find({});
     res.status(200).send(users);
 })
 
-app.get("/urls", async (req, res) => {
+app.get("/allurls", async (req, res) => {
     const urls = await Url.find({});
     res.status(200).send(urls);
     
 });
 
-app.get("/qrcodes", async (req, res) => {
+app.get("/allqrcodes", async (req, res) => {
     const qrcodes = await Qrcode.find({});
     res.status(200).send(qrcodes);
 });
